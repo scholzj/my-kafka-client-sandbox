c481cf971b25ac4c7a327e6088ea53d3edef504d
Jakub Scholz <www@scholzj.com>
2017-09-13 16:47:19 +0200

    Add first code samples based on Kafka Producer and Consumer

---
+++ kafka-client/src/main/java/cz/scholz/kafka/kafkaclients/util/RandomStringGenerator.java
@@ -0,0 +1,19 @@
+package cz.scholz.kafka.kafkaclients.util;
+
+import java.util.Random;
+
+public class RandomStringGenerator {
+    private static String SALTCHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+    public static String getSaltString(int length) {
+
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < length) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+}
