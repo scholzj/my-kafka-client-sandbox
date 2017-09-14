3a2289c312d4618161900463232c3bca54fd268b
Jakub Scholz <www@scholzj.com>
2017-09-14 10:21:05 +0200

    Add async sender

---
+++ kafka-client/src/main/java/cz/scholz/kafka/kafkaclients/AsyncProducerCallback.java
@@ -0,0 +1,35 @@
+package cz.scholz.kafka.kafkaclients;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class AsyncProducerCallback implements Callback {
+    private final int messageNo;
+    private final Boolean debug;
+    private AtomicInteger inFlightMessagesCounter;
+
+    public AsyncProducerCallback(int messageNo, Boolean debug, AtomicInteger counter)
+    {
+        counter.incrementAndGet();
+        this.messageNo = messageNo;
+        this.debug = debug;
+        this.inFlightMessagesCounter = counter;
+    }
+
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if (exception == null)
+        {
+            inFlightMessagesCounter.decrementAndGet();
+
+            if (debug) {
+                System.out.println("-I- confirmed message no. " + messageNo + " to offset " + metadata.offset() + " and partition " + metadata.partition());
+            }
+        }
+        else
+        {
+            System.out.println("-E- Failed to receive ");
+        }
+    }
+}
---
+++ kafka-client/src/main/java/cz/scholz/kafka/kafkaclients/MessageProducerAsync.java
@@ -0,0 +1,61 @@
+package cz.scholz.kafka.kafkaclients;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import cz.scholz.kafka.kafkaclients.util.RandomStringGenerator;
+
+public class MessageProducerAsync {
+    private static int count = 10000;
+    private static int messageSize = 1024;
+
+    private static Boolean debug = false;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
+        System.setProperty("org.slf4j.simpleLogger.showThreadName", "false");
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        Date totalStartTime = new Date();
+        int messageNo = 0;
+        AtomicInteger inFlightMessagesCounter = new AtomicInteger(0);
+
+        while (messageNo < count)
+        {
+            messageNo++;
+
+            ProducerRecord record = new ProducerRecord<String, String>("myTopic", "MSG-" + messageNo, RandomStringGenerator.getSaltString(messageSize));
+            producer.send(record, new AsyncProducerCallback(messageNo, debug, inFlightMessagesCounter));
+
+            if (debug)
+            {
+                System.out.println("-I- sent message no. " + messageNo + ": " + record.key() + " / " + record.value());
+            }
+        }
+
+        while (inFlightMessagesCounter.get() != 0)
+        {
+            System.out.println("-W- Waiting for callbacks to complete (in flight callbacks: " + inFlightMessagesCounter + ")");
+            Thread.sleep(1000);
+        }
+
+        Date totalEndTime = new Date();
+
+        producer.close();
+
+        System.out.println("-I- ####################################");
+        System.out.println("-I- Total " + messageNo + " messages sent: avg " + ((float)messageNo/(totalEndTime.getTime()-totalStartTime.getTime())*1000) + " msg/s");
+    }
+}
