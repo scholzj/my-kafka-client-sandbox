9ff05ccdd06001dc6b6a2483aef25fb543bbce02
Jakub Scholz <www@scholzj.com>
2017-09-18 22:12:00 +0200

    Add consumer and producer for UUID Serde

---
+++ kafka-client/src/main/java/cz/scholz/kafka/kafkaclients/MessageConsumerUUID.java
@@ -0,0 +1,85 @@
+package cz.scholz.kafka.kafkaclients;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class MessageConsumerUUID {
+    private static int timeout = 5000;
+    private static int timeTick = 1000;
+
+    private static Boolean debug = true;
+
+    public static void main(String[] args)
+    {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
+        System.setProperty("org.slf4j.simpleLogger.showThreadName", "true");
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "MyJavaConsumer");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.UUIDDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<UUID, String> consumer = new KafkaConsumer<UUID, String>(props);
+        consumer.subscribe(Collections.singletonList("UUIDTopic"));
+
+        Date totalStartTime = new Date();
+        Date blockStartTime = new Date();
+        int messageNo = 0;
+        int size = 0;
+        int sizeBlock = 0;
+
+        while (true)
+        {
+            ConsumerRecords<UUID, String> records = consumer.poll(timeout);
+
+            if(records.isEmpty()) {
+                System.out.println("-I- No message in topic for " + timeout/1000 + " seconds. Finishing ...");
+                break;
+            }
+
+            for (ConsumerRecord<UUID, String> record : records)
+            {
+                size += record.serializedValueSize() + record.serializedKeySize();
+                sizeBlock += record.serializedValueSize() + record.serializedKeySize();
+                messageNo++;
+
+                if (debug)
+                {
+                    System.out.println("-I- received message no. " + messageNo + ": " + record.key().toString() + " / " + record.value());
+                }
+
+                if (messageNo % timeTick == 0) {
+                    Date blockEndTime = new Date();
+                    System.out.println("-I- " + messageNo + " messages received: " + ((float) timeTick / (blockEndTime.getTime() - blockStartTime .getTime()) * 1000) + " msg/s, " + ((float) sizeBlock / (blockEndTime.getTime() - blockStartTime .getTime()) * 1000 / 1000) + " kB/s, average size " + ((float)sizeBlock/(float)timeTick));
+                    blockStartTime = new Date();
+                    sizeBlock = 0;
+                }
+            }
+        }
+
+        Date blockEndTime = new Date();
+        Date totalEndTime = new Date();
+
+        consumer.close();
+
+        if (messageNo % timeTick != 0) {
+            System.out.println("-I- " + messageNo + " messages received: " + ((float)(messageNo % timeTick)/(blockEndTime.getTime()-blockStartTime.getTime()-timeout)*1000) + " msg/s, "	+ ((float) sizeBlock / (blockEndTime.getTime() - blockStartTime.getTime()) * 1000 / 1000) + " kB/s, average size " + ((float)sizeBlock/(float)(messageNo % timeTick)));
+        }
+
+        System.out.println("-I- ####################################");
+        System.out.println("-I- Total " + messageNo + " messages received: avg " + ((float)messageNo/(totalEndTime.getTime()-totalStartTime.getTime()-timeout)*1000) + " msg/s, " + ((float)size/(totalEndTime.getTime()-totalStartTime.getTime()-timeout)*1000 / 1000) + " kB/s, average size " + ((float)size/(float)messageNo));
+    }
+}
---
+++ kafka-client/src/main/java/cz/scholz/kafka/kafkaclients/MessageProducerUUID.java
@@ -0,0 +1,76 @@
+package cz.scholz.kafka.kafkaclients;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import cz.scholz.kafka.kafkaclients.util.RandomStringGenerator;
+
+public class MessageProducerUUID {
+    private static int count = 10;
+    private static int timeTick = 1000;
+    private static int messageSize = 50;
+
+    private static Boolean debug = false;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
+        System.setProperty("org.slf4j.simpleLogger.showThreadName", "false");
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.UUIDSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        Date totalStartTime = new Date();
+        Date blockStartTime = new Date();
+        int messageNo = 0;
+        int size = 0;
+        int sizeBlock = 0;
+
+        while (messageNo < count)
+        {
+            messageNo++;
+
+            ProducerRecord record = new ProducerRecord<UUID, String>("UUIDTopic", UUID.randomUUID(), RandomStringGenerator.getSaltString(messageSize));
+            RecordMetadata result = (RecordMetadata) producer.send(record).get();
+
+            size += result.serializedValueSize() + result.serializedKeySize();
+            sizeBlock += result.serializedValueSize() + result.serializedKeySize();
+
+            if (debug)
+            {
+                System.out.println("-I- sent message no. " + messageNo + " to offset " + result.offset() + ": " + record.key() + " / " + record.value());
+            }
+
+            if (messageNo % timeTick == 0) {
+                Date blockEndTime = new Date();
+                System.out.println("-I- " + messageNo + " messages sent: " + ((float) timeTick / (blockEndTime.getTime() - blockStartTime .getTime()) * 1000) + " msg/s, " + ((float) sizeBlock / (blockEndTime.getTime() - blockStartTime .getTime()) * 1000 / 1000) + " kB/s, average size " + ((float)sizeBlock/(float)timeTick));
+                blockStartTime = new Date();
+                sizeBlock = 0;
+            }
+        }
+
+        Date blockEndTime = new Date();
+        Date totalEndTime = new Date();
+
+        producer.close();
+
+        if (messageNo % timeTick != 0) {
+            System.out.println("-I- " + messageNo + " messages sent: " + ((float)(messageNo % timeTick)/(blockEndTime.getTime()-blockStartTime.getTime())*1000) + " msg/s, "	+ ((float) sizeBlock / (blockEndTime.getTime() - blockStartTime.getTime()) * 1000 / 1000) + " kB/s, average size " + ((float)sizeBlock/(float)(messageNo % timeTick)));
+        }
+
+        System.out.println("-I- ####################################");
+        System.out.println("-I- Total " + messageNo + " messages sent: avg " + ((float)messageNo/(totalEndTime.getTime()-totalStartTime.getTime())*1000) + " msg/s, " + ((float)size/(totalEndTime.getTime()-totalStartTime.getTime())*1000 / 1000) + " kB/s, average size " + ((float)size/(float)messageNo));
+    }
+
+
+}
