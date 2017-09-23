5589462d48b15d136e8906d000710e5df3bf0b77
Jakub Scholz <www@scholzj.com>
2017-09-23 15:09:40 +0200

    Generate separate keys for ext interface

--- ssl-ca/generate.sh
+++ ssl-ca/generate.sh
@@ -13,10 +13,20 @@ cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-0.json | cfssljson 
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-1.json | cfssljson -bare keys/server-1
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-2.json | cfssljson -bare keys/server-2
 
+# Generate server keys - ext
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-0.json | cfssljson -bare keys/server-ext-0
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-1.json | cfssljson -bare keys/server-ext-1
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-2.json | cfssljson -bare keys/server-ext-2
+
 # Generate full chain server CRTs
 cat keys/server-0.pem keys/ca.pem > keys/server-0-full-chain.pem
 cat keys/server-1.pem keys/ca.pem > keys/server-1-full-chain.pem
 cat keys/server-2.pem keys/ca.pem > keys/server-2-full-chain.pem
+
+# Generate full chain server CRTs - ext
+cat keys/server-ext-0.pem keys/ca.pem > keys/server-ext-0-full-chain.pem
+cat keys/server-ext-1.pem keys/ca.pem > keys/server-ext-1-full-chain.pem
+cat keys/server-ext-2.pem keys/ca.pem > keys/server-ext-2-full-chain.pem
 
 # Generate user keys
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem user1.json | cfssljson -bare keys/user1
@@ -30,16 +40,19 @@ keytool -importcert -keystore keys/truststore -storepass $PASSWORD -storetype JK
 openssl pkcs12 -export -out keys/server-0.p12 -in keys/server-0-full-chain.pem -inkey keys/server-0-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/server-1.p12 -in keys/server-1-full-chain.pem -inkey keys/server-1-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/server-2.p12 -in keys/server-2-full-chain.pem -inkey keys/server-2-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/server-ext-0.p12 -in keys/server-ext-0-full-chain.pem -inkey keys/server-ext-0-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/server-ext-1.p12 -in keys/server-ext-1-full-chain.pem -inkey keys/server-ext-1-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/server-ext-2.p12 -in keys/server-ext-2-full-chain.pem -inkey keys/server-ext-2-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/user1.p12 -in keys/user1.pem -inkey keys/user1-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/user2.p12 -in keys/user2.pem -inkey keys/user2-key.pem -password pass:$PASSWORD
 
 # Convert PKCS12 keys to keystores
 rm keys/*.keystore
-#keytool -importcert -keystore server-0.keystore -storepass $PASSWORD -storetype JKS -alias ca -file ca.pem -noprompt
 keytool -importkeystore -srckeystore keys/server-0.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-0.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
-#keytool -importcert -keystore server-1.keystore -storepass $PASSWORD -storetype JKS -alias ca -file ca.pem -noprompt
 keytool -importkeystore -srckeystore keys/server-1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
-#keytool -importcert -keystore server-2.keystore -storepass $PASSWORD -storetype JKS -alias ca -file ca.pem -noprompt
 keytool -importkeystore -srckeystore keys/server-2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/server-ext-0.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-0.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/server-ext-1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/server-ext-2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/user1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/user1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/user2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/user2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
