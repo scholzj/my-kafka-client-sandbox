fb3a26ef144b5fc34160df4179cd2165f7ce1ff8
Jakub Scholz <www@scholzj.com>
2017-10-30 16:39:20 +0100

    Generate additional keys for Kafka Connect REST API

--- ssl-ca/generate.sh
+++ ssl-ca/generate.sh
@@ -18,6 +18,11 @@ cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-0.json | cfsslj
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-1.json | cfssljson -bare keys/server-ext-1
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem server-ext-2.json | cfssljson -bare keys/server-ext-2
 
+# Generate Connect keys - ext
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem connect-0.json | cfssljson -bare keys/connect-0
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem connect-1.json | cfssljson -bare keys/connect-1
+cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem connect-2.json | cfssljson -bare keys/connect-2
+
 # Generate full chain server CRTs
 cat keys/server-0.pem keys/ca.pem > keys/server-0-full-chain.pem
 cat keys/server-1.pem keys/ca.pem > keys/server-1-full-chain.pem
@@ -27,6 +32,11 @@ cat keys/server-2.pem keys/ca.pem > keys/server-2-full-chain.pem
 cat keys/server-ext-0.pem keys/ca.pem > keys/server-ext-0-full-chain.pem
 cat keys/server-ext-1.pem keys/ca.pem > keys/server-ext-1-full-chain.pem
 cat keys/server-ext-2.pem keys/ca.pem > keys/server-ext-2-full-chain.pem
+
+# Generate full chain Connecr CRTs
+cat keys/connect-0.pem keys/ca.pem > keys/connect-0-full-chain.pem
+cat keys/connect-1.pem keys/ca.pem > keys/connect-1-full-chain.pem
+cat keys/connect-2.pem keys/ca.pem > keys/connect-2-full-chain.pem
 
 # Generate user keys
 cfssl gencert -ca keys/ca.pem -ca-key keys/ca-key.pem user1.json | cfssljson -bare keys/user1
@@ -44,6 +54,9 @@ openssl pkcs12 -export -out keys/server-2.p12 -in keys/server-2-full-chain.pem -
 openssl pkcs12 -export -out keys/server-ext-0.p12 -in keys/server-ext-0-full-chain.pem -inkey keys/server-ext-0-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/server-ext-1.p12 -in keys/server-ext-1-full-chain.pem -inkey keys/server-ext-1-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/server-ext-2.p12 -in keys/server-ext-2-full-chain.pem -inkey keys/server-ext-2-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/connect-0.p12 -in keys/connect-0-full-chain.pem -inkey keys/connect-0-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/connect-1.p12 -in keys/connect-1-full-chain.pem -inkey keys/connect-1-key.pem -password pass:$PASSWORD
+openssl pkcs12 -export -out keys/connect-2.p12 -in keys/connect-2-full-chain.pem -inkey keys/connect-2-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/user1.p12 -in keys/user1.pem -inkey keys/user1-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/user2.p12 -in keys/user2.pem -inkey keys/user2-key.pem -password pass:$PASSWORD
 openssl pkcs12 -export -out keys/user-connect.p12 -in keys/user-connect.pem -inkey keys/user-connect-key.pem -password pass:$PASSWORD
@@ -56,6 +69,9 @@ keytool -importkeystore -srckeystore keys/server-2.p12 -srcstoretype PKCS12 -src
 keytool -importkeystore -srckeystore keys/server-ext-0.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-0.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/server-ext-1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/server-ext-2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/server-ext-2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/connect-0.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/connect-0.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/connect-1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/connect-1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
+keytool -importkeystore -srckeystore keys/connect-2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/connect-2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/user1.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/user1.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/user2.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/user2.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore keys/user-connect.p12 -srcstoretype PKCS12 -srcstorepass $PASSWORD -destkeystore keys/user-connect.keystore -deststoretype JKS -deststorepass $PASSWORD -noprompt
