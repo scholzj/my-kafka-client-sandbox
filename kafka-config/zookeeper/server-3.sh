3386ed802baa0fba15b37220d3b7af4c37c3303a
Jakub Scholz <www@scholzj.com>
2017-11-27 09:32:23 +0100

    Fix the ZK startup scripts

--- kafka-config/zookeeper/server-3.sh
+++ kafka-config/zookeeper/server-3.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-CONFIG_DIR="$(dirname ${BASH_SOURCE[0]})"
+ZOOKEEPER_NODE="3"
 
-SERVER_JVMFLAGS="-Djava.security.auth.login.config=${CONFIG_DIR}/jaas.config"; \
-    bin/zkServer.sh start ${CONFIG_DIR}/zookeeper-3.properties
+CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f /tmp/zookeeper-${ZOOKEEPER_NODE}/myid ]; then
+    echo "Creating myid file"
+    cp -r ${CONFIG_DIR}/tmp/zookeeper-${ZOOKEEPER_NODE} /tmp/
+fi
+
+export SERVER_JVMFLAGS="-Djava.security.auth.login.config=${CONFIG_DIR}/jaas.config"; \
+    bin/zkServer.sh start-foreground ${CONFIG_DIR}/zookeeper-${ZOOKEEPER_NODE}.properties
